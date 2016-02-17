60f80a6ccdbca49d952740018b9b850019c52e2b
Luis Gerardo <luisgdm93@gmail.com>
2016-02-17 11:41:05 -0600

    EVERYTHING

---
+++ testAppv01/register.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace WorkingWithNavigation
+{
+	public class Register : ContentPage
+	{
+		public Register ()
+		{
+			Label Registrate = new Label {
+				Text = "Regístrate",
+				TextColor = Color.Black,
+				FontAttributes = FontAttributes.Bold
+			};
+
+			Label IngresaDat = new Label {
+				Text = "Ingresa los datos requeridos",
+				FontAttributes= FontAttributes.Bold,
+
+			};
+
+			Button FacebookAcc = new Button {
+				Text = "Facebook",
+				TextColor = Color.White,
+				BackgroundColor = Color.FromRgb(37,73,149),
+				WidthRequest = 290
+			};
+
+			Button GoogleAcc = new Button {
+				Text = "Google",
+				TextColor = Color.White,
+				BackgroundColor = Color.FromRgb(220,40,28),
+				WidthRequest = 290,
+
+			};
+			Entry Nombre = new Entry {
+				WidthRequest = 290,
+				Placeholder = "Nombre",
+				TextColor = Color.FromRgba(0,0,0,50),
+				HeightRequest = 40
+			};
+
+			Entry Apellido = new Entry {
+				WidthRequest = 290,
+				Placeholder = "Apellido",
+				TextColor = Color.FromRgba(0,0,0,50),
+				HeightRequest = 40
+			};
+
+			Entry Celular = new Entry {
+				WidthRequest = 290,
+				Placeholder = "Celular",
+				TextColor = Color.FromRgba(0,0,0,50),
+				HeightRequest = 40
+			};
+
+			Entry Correo = new Entry {
+				WidthRequest = 290,
+				Placeholder = "Correo",
+				TextColor = Color.FromRgba(0,0,0,50),
+				HeightRequest = 40
+			};
+
+			Button Registrar = new Button {
+				Text = "Registrar",
+				WidthRequest = 290,
+				BackgroundColor = Color.FromRgb(210,34,70),
+				TextColor = Color.White
+			};
+
+
+			RelativeLayout relLayout2 = new RelativeLayout();
+
+			relLayout2.Children.Add ((Registrate), 
+				Constraint.RelativeToParent ((parent) => {
+					return parent.Width/2 - Registrate.Width/2;
+				}),
+				Constraint.RelativeToParent((parent) => {
+					return 10;
+				}));
+
+			relLayout2.Children.Add ((FacebookAcc), 
+				Constraint.RelativeToParent ((parent) => {
+					return parent.Width/2 - FacebookAcc.Width/2;
+				}),
+				Constraint.RelativeToParent((parent) => {
+					return parent.Height/7;
+				}));
+
+			relLayout2.Children.Add ((GoogleAcc),
+				Constraint.RelativeToParent ((parent) => {
+					return parent.Width / 2 - GoogleAcc.Width / 2;
+				}),
+				Constraint.RelativeToParent ((parent) => {
+					return parent.Height / 7 + FacebookAcc.Height + 10;
+				}));
+
+			relLayout2.Children.Add ((IngresaDat),
+				Constraint.RelativeToParent ((parent) => {
+					return FacebookAcc.X;
+				}),
+				Constraint.RelativeToParent ((parent) => {
+					return GoogleAcc.Y + 80;
+				}));
+
+			relLayout2.Children.Add ((Nombre),
+				Constraint.RelativeToParent ((parent) => {
+					return parent.Width / 2 - Nombre.Width / 2;
+				}),
+				Constraint.RelativeToParent ((parent) => {
+					return IngresaDat.Y + 50;
+				}));
+
+			relLayout2.Children.Add ((Apellido),
+				Constraint.RelativeToParent ((parent) => {
+					return parent.Width / 2 - Apellido.Width / 2;
+				}),
+				Constraint.RelativeToParent ((parent) => {
+					return Nombre.Y + 40;
+				}));
+
+			relLayout2.Children.Add ((Celular),
+				Constraint.RelativeToParent ((parent) => {
+					return parent.Width / 2 - Celular.Width / 2;
+				}),
+				Constraint.RelativeToParent ((parent) => {
+					return Apellido.Y + 40;
+				}));
+
+			relLayout2.Children.Add ((Correo),
+				Constraint.RelativeToParent ((parent) => {
+					return parent.Width / 2 - Correo.Width / 2;
+				}),
+				Constraint.RelativeToParent ((parent) => {
+					return Celular.Y + 40;
+				}));
+
+			relLayout2.Children.Add ((Registrar),
+				Constraint.RelativeToParent ((parent) => {
+					return parent.Width / 2 - Registrar.Width / 2;
+				}),
+				Constraint.RelativeToParent ((parent) => {
+					return Correo.Y + 60;
+				}));
+
+
+			//para que se centre en el relativelayout (tomando en cuenta el width del label,etc)
+			Registrate.SizeChanged += ((sender, eventArgs) =>
+				{
+					relLayout2.ForceLayout();
+				});
+
+			Content = relLayout2;
+			Padding = new Thickness (0, Device.OnPlatform (20, 0, 0), 0, 0);
+
+		}
+
+	}
+}
+
---
+++ testAppv01/welcome.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace WorkingWithNavigation
+{
+	public class welcome : ContentPage
+	{
+		public welcome ()
+		{
+			Label header = new Label {
+				Text = "Xylo",
+				FontSize = 70,
+				FontAttributes= FontAttributes.Bold,
+				FontFamily="Avenir"
+			};
+
+			Button IniciarSes = new Button {
+				Text = "Iniciar Sesión",
+				TextColor = Color.White,
+				BackgroundColor = Color.FromRgb(255,135,20),
+				WidthRequest = 275
+			};
+
+			Button Registrate = new Button {
+				Text = "Regístrate",
+				TextColor = Color.White,
+				BackgroundColor = Color.FromRgb (245, 104, 78),
+				WidthRequest = 275,
+
+			};
+
+			RelativeLayout relLayout1 = new RelativeLayout();
+
+			relLayout1.Children.Add(header,
+				Constraint.RelativeToParent((parent) => {
+					return (parent.Width / 2) - (header.Width/2.0);
+				}),
+				Constraint.RelativeToParent ((parent) => {
+					return parent.Height/3 + 20;
+				}));
+
+			relLayout1.Children.Add (Registrate,
+				Constraint.RelativeToParent ((parent) => {
+					return (parent.Width / 2) - (Registrate.Width/2.0);
+				}),
+				Constraint.RelativeToParent((parent)=> {
+					return parent.Height*0.75 - IniciarSes.Height - 10;
+				}));
+
+			relLayout1.Children.Add (IniciarSes,
+				Constraint.RelativeToParent ((parent) => {
+					return (parent.Width / 2) - (IniciarSes.Width/2.0);
+				}),
+				Constraint.RelativeToParent((parent)=> {
+					return parent.Height*0.75;
+				}));
+
+			//para que se centre en el relativelayout (tomando en cuenta el width del label,etc)
+			header.SizeChanged += ((sender, eventArgs) =>
+				{
+					relLayout1.ForceLayout();
+				});
+
+			Registrate.Clicked += OnNextPageButtonClicked;
+
+			Content = new StackLayout { 
+				Children = {
+					relLayout1
+				}
+			};
+		}
+
+		async void OnNextPageButtonClicked (object sender, EventArgs e)
+		{
+			await Navigation.PushAsync (new Register ());
+		}
+	}
+}
